c72b9a19b8c3cbba707ac3c5e1b9fbe1d1992a9d
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-05-21 16:29:29 +0800

    Make this work with ollama and llama3 locally without requiring openai

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
-import os
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings.sentence_transformer import (
+    SentenceTransformerEmbeddings,
+)
+from langchain_community.llms import Ollama
+
 from pdf_reader import load_pdf
 from read_job_posting import extract_text_from_url
 from splitter import split_text_documents
 
-
-def get_cover_letter(url, pdf, openai_api_key):
+def get_cover_letter(url, pdf):
 
     pdf_doc = load_pdf(pdf)
     job_post = extract_text_from_url(url)
@@ -16,10 +17,17 @@ def get_cover_letter(url, pdf, openai_api_key):
     pdf_doc.extend(job_post)
     documents = split_text_documents(pdf_doc)
 
-    vectordb = Chroma.from_documents(documents, embedding=OpenAIEmbeddings(openai_api_key = openai_api_key))
+    embedding_function = SentenceTransformerEmbeddings(
+        model_name="all-MiniLM-L6-v2"
+    )
+    vectordb = Chroma.from_documents(documents, embedding=embedding_function)
+
+    llm = Ollama(
+        model="llama3",
+    )
 
     pdf_qa = RetrievalQA.from_chain_type(
-        ChatOpenAI(temperature=0.7, model_name='gpt-3.5-turbo', openai_api_key = openai_api_key),
+        llm,
         retriever=vectordb.as_retriever(search_kwargs={'k': 6}),
         chain_type="stuff",
     )
