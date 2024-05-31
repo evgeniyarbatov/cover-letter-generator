c34c36f3c661b4221e5750751b09f561abd05da5
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-05-31 10:26:54 +0800

    Select LLM based on machine I am running on

--- llm.py
+++ llm.py
@@ -1,7 +1,9 @@
+import socket
+
 from langchain.chains import RetrievalQA
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings.sentence_transformer import (
-    SentenceTransformerEmbeddings,
+  SentenceTransformerEmbeddings,
 )
 from langchain_community.llms import Ollama
 from langchain.prompts import PromptTemplate
@@ -23,53 +25,65 @@ CV: ```
 Job description: <{job_description}>
 """
 
+def get_llm():
+  hostname = socket.gethostname()
+  match hostname:
+    case "Evgenys-iMac.local":
+      return Ollama(
+          model="llama3",
+      )
+    case "HangLe-MacBook.local":
+      return Ollama(
+          model="phi3",
+      )
+    case _:
+      raise Exception(f"{hostname} is unsupported")     
+
 def get_cover_letter(
-    job_url, 
-    cv_pdf,
-    additional_pdfs,
+  job_url, 
+  cv_pdf,
+  additional_pdfs,
 ):
-    job_description = url_text(job_url)
-    cv = pdf_text(cv_pdf)
+  job_description = url_text(job_url)
+  cv = pdf_text(cv_pdf)
 
-    documents = []
-    documents.extend(get_documents(cv))
-    documents.extend(get_documents(job_description))
+  documents = []
+  documents.extend(get_documents(cv))
+  documents.extend(get_documents(job_description))
 
-    for additional_pdf in additional_pdfs:
-        additional_text = pdf_text(additional_pdf)
-        documents.extend(get_documents(additional_text))
+  for additional_pdf in additional_pdfs:
+    additional_text = pdf_text(additional_pdf)
+    documents.extend(get_documents(additional_text))
 
-    embedding_function = SentenceTransformerEmbeddings(
-        model_name="all-MiniLM-L6-v2"
-    )
+  embedding_function = SentenceTransformerEmbeddings(
+    model_name="all-MiniLM-L6-v2"
+  )
 
-    vectordb = Chroma.from_documents(documents, embedding=embedding_function)
+  vectordb = Chroma.from_documents(documents, embedding=embedding_function)
 
-    llm = Ollama(
-        model="llama3",
-    )
+  llm = get_llm()
 
-    prompt = PromptTemplate(
-        input_variables=["context", "query"],
-        template=PROMPT_TEMPLATE,
-        partial_variables={
-            'cv': cv,
-            'job_description': job_description,
-        }
-    )
+  prompt = PromptTemplate(
+    input_variables=["context", "query"],
+    template=PROMPT_TEMPLATE,
+    partial_variables={
+      'cv': cv,
+      'job_description': job_description,
+    }
+  )
 
-    qa = RetrievalQA.from_chain_type(
-        llm,
-        retriever=vectordb.as_retriever(),
-        return_source_documents=True,
-        chain_type="stuff", 
-        chain_type_kwargs={
-            "prompt": prompt,
-        }
-    )
+  qa = RetrievalQA.from_chain_type(
+    llm,
+    retriever=vectordb.as_retriever(),
+    return_source_documents=True,
+    chain_type="stuff", 
+    chain_type_kwargs={
+      "prompt": prompt,
+    }
+  )
 
-    result = qa({
-        "query": "Write a cover letter for given CV and Job posting in a conversational style"
-    })
+  result = qa({
+    "query": "Write a cover letter for given CV and Job posting in a conversational style"
+  })
 
-    return result
+  return result
